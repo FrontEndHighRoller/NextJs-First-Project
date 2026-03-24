68f0c6ec6010c6cf40ff9837cebfb22b906910e1
Dennis <142683382+FrontEndHighRoller@users.noreply.github.com>
2026-03-24 09:20:13 +0000

    catch errors

--- app/dashboard/invoices/[id]/edit/page.tsx
+++ app/dashboard/invoices/[id]/edit/page.tsx
@@ -4,6 +4,7 @@ import {
   fetchInvoiceById,
   fetchCustomers,
 } from "@/app/lib/data";
+import { notFound } from "next/navigation";
 
 export default async function Page(props: {
   params: Promise<{ id: string }>;
@@ -14,6 +15,10 @@ export default async function Page(props: {
     fetchInvoiceById(id),
     fetchCustomers(),
   ]);
+  if (!invoice) {
+    notFound();
+  }
+
   return (
     <main>
       <Breadcrumbs
